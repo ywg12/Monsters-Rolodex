59e4413de3718d2d4082d9f70ef48a0a4b91ad13
Yashwardhan Gaur <yashwardhangaur1204@gmail.com>
2023-09-26 18:16:52 +0530

    Optimization

--- src/App.js
+++ src/App.js
@@ -18,20 +18,22 @@ class App extends Component{
 			() => {}
 			))
 	}
+	onSearchChange = (event) => {
+		const searchString = event.target.value.toLocaleLowerCase();
+		this.setState(() => {
+			return {searchField :searchString}
+		});
+	}
 	render(){
-		const filteredMosters = this.state.monsters.filter((monster) => {
-			return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+		const filteredMosters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
 		})
 		
 		return (
 			<div className="App">
-				<input className='search-box' type='search' placeholder='Search Monsters' onChange={(event) => {						
-						const searchString = event.target.value.toLocaleLowerCase();
-						this.setState(() => {
-							return {searchField :searchString}
-						});
-						
-				}}>
+				<input className='search-box' type='search' placeholder='Search Monsters' onChange={onSearchChange}>
 
 				</input>
 				{
